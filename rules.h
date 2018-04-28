4dcac74b93910f1f62775c6f161661551407dc89
lherreranetlink <leonardoherreraornelas960302@gmail.com>
2018-04-28 11:05:52 -0500

    add structs of some grammar rules

---
+++ rules.h
@@ -0,0 +1,40 @@
+#ifndef RULES_H_INCLUDED
+#define RULES_H_INCLUDED
+#include "token.h"
+
+typedef enum {FUNCTION_PARAM, FUNCTION_DEFINITION, VARIABLE_DEFINITION, SIMPLE_TOKEN} RuleType;
+
+typedef struct FunctionParam
+{
+    Token* dataType;
+    Token* identifier;
+} FunctionParam;
+
+typedef struct FunctionDefinition
+{
+    Token* dataType;
+    Token* identifier;
+    Param* paramList;
+} FunctionDefinition;
+
+typedef struct VariableDefinition
+{
+    Token* dataType;
+    Token* tokenList;
+} VariableDefinition;
+
+typedef struct GenericSyntaxTreeNode
+{
+    int stateToSee;
+    RuleType ruleType;
+    union
+    {
+        VariableDefinition* varDef;
+        FunctionDefinition* functionDef;
+        FunctionParam* functionParam;
+        Token* token;
+    } attr;
+} GenericSyntaxTreeNode;
+
+
+#endif // RULES_H_INCLUDED
